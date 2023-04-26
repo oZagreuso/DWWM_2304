d4a07f1dfff99becfb1383fc016ad6fd1f67140c
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-04-26 16:41:42 +0200

    UpdateBicyclette_PM_16h40_260423

---
+++ maBicyclette/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Boolean beauTemps;
+        Boolean baladeBicyclette;
+        Boolean bicycletteHS;
+        Boolean allerGaragsite;
+        Boolean allerEtang;
+        Boolean lireGoT;
+        Boolean allerBibliothèque;
+        Boolean livreIndisponible;
+        Boolean emprunterRomanPolicier;
+        Boolean rentrerMaison;
+        Boolean allerFauteuil;
+        Boolean livreTrouvé;
+
+        Console.WriteLine(" La bicyclette ");
+
+        if (bool.beauTemps == true)
+
+    }
+}
