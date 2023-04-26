13b4c3de2ee9608a797662d378c8858de0a0a948
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-04-26 15:36:50 +0200

    UpdatePM_15h35_260423

--- triNombresPlusPlus/Program.cs
+++ triNombresPlusPlus/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Security.Cryptography;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        double a;
-        double b;
-        double c;
+        int a;
+        int b;
+        int c;
         string saisie;
 
         Console.WriteLine(" Programme de tri de nombres Upgrade ");
@@ -17,6 +18,7 @@ internal class Program
         saisie = Console.ReadLine();
 
         a = int.Parse(saisie);
+
 
         Console.WriteLine(" Veuillez saisir un nombre b ");
 
@@ -30,45 +32,74 @@ internal class Program
 
         c = int.Parse(saisie);
 
-        if (a < b)
+        if (a == b)
         {
-            if (b < c)
+            if (a == c)
             {
-                Console.WriteLine(a + " < " + b + " < " + c);
-
+                Console.WriteLine(a + " = " + b + " = " + c);
             }
-            else
+            if (a < c)
             {
-                if (a < c)
+                Console.WriteLine(a + " = " + b + " < " + c);
+            }
+            if (c < a)
+            {
+                Console.WriteLine(c + " < " + a + " = " + b);
+            }
+        }
+        if  (a < b)
+        {
+            if (a == c)
+            {
+                Console.WriteLine(a + " = " + c + " < " + b);
+            }
+            if (a < c)
+            {
+                if (b == c)
+                {
+                    Console.WriteLine(a + " < " + b + " = " + c);
+                }
+                if (b < c)
+                {
+                    Console.WriteLine(a + " < " + b + " < " + c);
+                }
+                if (c < b)
                 {
                     Console.WriteLine(a + " < " + c + " < " + b);
                 }
-                else
+                if (c < a)
                 {
                     Console.WriteLine(c + " < " + a + " < " + b);
                 }
             }
         }
-        else
+        if (b < a)
         {
-            if (a < c)
+            if ( a == c)
+            {
+                Console.WriteLine(b + " < " + a + " = " + c);
+            }
+            if  (a < c)
             {
                 Console.WriteLine(b + " < " + a + " < " + c);
             }
-            else
+        }
+        if (c < a)
+        {
+            if ( b == c)
             {
-                if (b < c)
-                {
-                    Console.WriteLine(b + " < " + c + " < " + a);
-                }
-                else
-                {
-                    Console.WriteLine(c + " < " + b + " < " + a);
-                }
-
-
-
+                Console.WriteLine(b + " = " + c + " < " + a);
+            }
+            if (b < c)
+            {
+                Console.WriteLine(b + " < " + c + " < " + a);
+            }
+            if (c < b)
+            {
+                Console.WriteLine(c + " < " + b + " < " + a);
             }
         }
     }
+
 }
+
