5f11f6fdf5ead6388fb0c62967eb6dd6de7101a3
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-04-27 16:59:53 +0200

    UpdateFinalPM_270423

---
+++ ControleSaisieAvecLimite/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+
+namespace ControleSaisieAvecLimite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = " formation ";
+            string saisie;
+            int compteur = 0;
+
+            do
+            {
+                Console.WriteLine(" Saisir le mot-de-passe ");
+
+                saisie = Console.ReadLine();
+                                             
+                compteur++;                             
+
+            }
+            while (compteur <= 3);
+            {
+                do
+                {
+                    Console.WriteLine(" vous êtes connecté ");
+                }
+                while (password == saisie);
+                {
+
+                }
+            }
+
+
+
+            
+        }
+    }
+}
