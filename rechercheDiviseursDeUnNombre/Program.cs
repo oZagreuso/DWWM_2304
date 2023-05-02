46184ef8ab3c10b1563777486066cf8bb8e0d6cb
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-02 11:30:38 +0200

    UpdateAM11h30_020523

---
+++ rechercheDiviseursDeUnNombre/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace rechercheDiviseursDeUnNombre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int i;
+
+            Console.WriteLine(" Programme de recherche des diviseurs d'un nombre ");
+            Console.WriteLine(" Saisir nombre ");
+            
+            N = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Les diviseurs de " + N + " sont ");
+
+            for (i = 2; i < N; i += 2)
+            {
+                if (N % i == 0)
+                {
+                    Console.WriteLine( + i);
+                }
+            }
+        }
+    }
+}
