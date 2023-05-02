41e416458850c23aa61c6273aa0dd7b9f77fe3ef
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-02 10:47:56 +0200

    UpdateAM10h47_020523

---
+++ intervalleDeuxNombres/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+
+namespace intervalleDeuxNombres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int saisie;
+            int saisie2;         
+
+            Console.WriteLine(" Saisir un nombre entier ");
+            saisie = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(" Saisir un second nombre entier ");
+            saisie2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" L'intervalle entre A et B se compose de " );
+
+            if (saisie < saisie2)
+            {
+                for (int i = saisie + 1; i < saisie2; i++)
+                {
+                    Console.WriteLine(i);                  
+                }
+            }
+            else
+            {
+                for (int i = saisie - 1; i > saisie2; i--)
+                {
+                    Console.WriteLine(i);              
+                }
+            }
+
+        }
+    }
+}
