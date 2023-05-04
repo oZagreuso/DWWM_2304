22ab86c4eae1de7065da29ad60bc57915d08c805
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-04 09:45:53 +0200

    UpdateAM9h45_040523

--- recherhcerUnNombreDansUnTableau/Program.cs
+++ recherhcerUnNombreDansUnTableau/Program.cs
@@ -3,8 +3,27 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {         
+            string[] monTableau;
+            monTableau = new string[7];
+            monTableau[0] = "8";
+            monTableau[1] = "16";
+            monTableau[2] = "32";
+            monTableau[3] = "64";
+            monTableau[4] = "128";
+            monTableau[5] = "512";
+
+            for (int i = 0; i < monTableau.Length; i++)
+            {
+                Console.WriteLine(monTableau[i]);
+                Console.WriteLine(" saisir un nombre ");
+                string saisie = Console.ReadLine();
+                
+                foreach (String s in monTableau)
+                {
+                    Console.WriteLine(s);
+                }                  
+            }
         }
     }
 }
