ef8ff1b8050e0c19265dbd63c1a095e29628b299
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-05 08:24:07 +0200

    UpdateAM8h23_050523

--- barnabeFaitSesCourses/Program.cs
+++ barnabeFaitSesCourses/Program.cs
@@ -1,12 +1,13 @@
-﻿namespace barnabeFaitSesCourses
+﻿using System;
+
+namespace barnabeFaitSesCourses
 {
     internal class Program
     {
         static void Main(string[] args)
         {                    
-            double solde;
-            double depenses = 0;
-            int magasinVisite = 0;
+            double solde;            
+            int magasinVisite = 1;
 
             Console.WriteLine(" Barnabé fait ses courses ");
             Console.WriteLine(" indiquer le solde initial du compte de Barnabé ");
@@ -14,12 +15,20 @@
 
             while (solde >= ((solde / 2) + 1))
             {
+            double depenses = 0;
             depenses = ((solde / 2) + 1);
             solde = solde - depenses;                
             magasinVisite++;
             }          
-                Console.WriteLine(" le nombre de magasins visités est de " + magasinVisite);
-            
+            Console.WriteLine(" le nombre de magasins visités est de " + magasinVisite);
+            if (solde > 0)
+            {
+                Console.WriteLine(" Barnabé a dépensé " + solde + " euros dans un seul magasin ");
+            }
+            if (solde == 0)
+            {
+                Console.WriteLine(" mais Barnabé n'a pas d'argent, il ne peut pas faire de shopping ");
+            }
         }
     }
 }
