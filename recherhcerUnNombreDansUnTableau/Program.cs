c3c93c0fb7384e9629999599f6ca6fe5dc66f800
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-09 09:13:47 +0200

    UpdateAM09h13_090523

--- recherhcerUnNombreDansUnTableau/Program.cs
+++ recherhcerUnNombreDansUnTableau/Program.cs
@@ -8,35 +8,33 @@ namespace recherhcerUnNombreDansUnTableau
         {
             int[] monTableau;
             int saisie;
-            monTableau = new int[6];
+            monTableau = new int[7];
             monTableau[0] = 8;
             monTableau[1] = 16;
             monTableau[2] = 32;
             monTableau[3] = 64;
             monTableau[4] = 128;
-            monTableau[5] = 512;
+            monTableau[5] = 256;
+            monTableau[6] = 512;
 
+           
             for (int i = 0; i < monTableau.Length; i++)
             {
                 Console.WriteLine(" Saisir un nombre ");
                 saisie = int.Parse(Console.ReadLine());
 
-                foreach (int s in monTableau)
+                if (saisie == monTableau[i])
                 {
-                    if (saisie == s)
-                    {
-
-                        Console.WriteLine(monTableau[i]);
-
-                    }
-
-                    else
-                    {
-                        Console.WriteLine(" Nombre non trouvé ");
-                    }
+                    Console.WriteLine(" La valeur de l'indice est " + i);
+                }
+                else
+                {
+                    Console.WriteLine(" Nombre non trouvé ");
                 }
             }
         }
     }
 }
+    
+
     
