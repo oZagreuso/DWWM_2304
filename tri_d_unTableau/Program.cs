3e22866a0c93a725248bb1d0e36dc33b30d7c2e8
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-15 16:23:37 +0200

    Update16h23_150523

---
+++ tri_d_unTableau/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+
+namespace tri_d_unTableau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] triTab = new int[] { 128, 64, 8, 512, 16, 32, 256 };
+            Console.WriteLine(" Programme de tri d'un tableau ");
+            Console.WriteLine(" Le Tableau initial, de l'index 0 à 6, comportant dans l'ordre 128, 64, 8, 512, 16, 32, 256, se présentera, après tri, ainsi: ");
+
+            for (int i = 0; i < triTab.Length; i++)
+            {
+                int indiceMin = i;
+
+                for (int j = i + 1; j < triTab.Length; j++)
+                {
+                    if (triTab[j] < triTab[indiceMin])
+                    {
+                        indiceMin = j;
+                    }
+                }
+                if (indiceMin != i)
+                {
+                    int temp = triTab[i];
+                    triTab[i] = triTab[indiceMin];
+                    triTab[indiceMin] = temp;
+                }
+            }
+            for (int k = 0; k < triTab.Length; k++)
+            {
+                Console.WriteLine(triTab[k]);
+            }
+
+        }
+    }
+}
