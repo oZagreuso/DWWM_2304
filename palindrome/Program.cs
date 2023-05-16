37b509e1bc37b0a2e494f1b039a9386b5c7c793d
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-16 15:10:27 +0200

    UpdatePM_15h10_160523

--- palindrome/Program.cs
+++ palindrome/Program.cs
@@ -5,34 +5,58 @@
         static void Main(string[] args)
         {
             Console.WriteLine(" Programme de detection de palindrome dans une phrase");
+            string saisiePhrase = "";
+            bool estPalindrome = true;
+            bool conditionsPalindromeOk = true;          
 
-            Console.Write(" Entrez une phrase se terminant par un point ");
-            string saisiePhrase = Console.ReadLine();
-
-
-            if (saisiePhrase == ".")
+            do
             {
-                Console.WriteLine(" La saisie est inexacte, elle est seulement composée d'un point, nouvelle saisie requise ");
+                Console.Write(" Entrez une phrase se terminant par un point ");
+                saisiePhrase = Console.ReadLine();
+
+
+                if (saisiePhrase == "")
+                {
+                    Console.WriteLine(" La saisie est inexacte, le champ est vide, nouvelle saisie requise ");
+                   
+                    conditionsPalindromeOk = false;
+                }              
+                else if (saisiePhrase == ".")
+                {
+                    Console.WriteLine(" La saisie est inexacte, nouvelle saisie requise ");
+                    conditionsPalindromeOk = false;
+                }
+                else if (saisiePhrase[saisiePhrase.Length - 1] != '.') // SI le dernier caractère, du tableau saisiePhrase, n'est pas un point = extraction de caractère (élément dans le cas d'un tableau)
+                {
+                    conditionsPalindromeOk = false;
+                    Console.WriteLine(" La saisie est inexacte, la chaîne de caractères ne se termine pas par un point, nouvelle saisie requise ");
+                }
+                else
+                {
+                    conditionsPalindromeOk = true;
+                }
             }
+            while (!conditionsPalindromeOk);
 
-            if (saisiePhrase.Length == 0)
+            for (int i = 0; i < saisiePhrase.Length / 2d; i++)
             {
-                Console.WriteLine(" La saisie est vide ");
+                if (saisiePhrase[i] != saisiePhrase[saisiePhrase.Length - 2 - i])
+                {
+                    estPalindrome = false;
+                   
+                }              
+
+            }
+            if (estPalindrome)
+            {
+                Console.WriteLine(" La chaîne de caractères est un palindrome ");
             }
             else
             {
-
-                int phraseLongueur = saisiePhrase.Length;
-                for (int i = 0; i < phraseLongueur / 2; i++)
-                {
-                    if (saisiePhrase[i] != saisiePhrase[phraseLongueur - 1 - i])
-                    {
-                        Console.WriteLine(" La chaîne de caractère n'est pas un palindrome");
-                    }
-                }
-                Console.WriteLine(" La chaîne de caractères est un palindrome ");
+                Console.WriteLine(" La chaîne de caractères n'est pas un palindrome ");
             }
         }
     }
-
 }
+
+
