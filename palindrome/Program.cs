60354113a20e6448b85aa9dacbd6c759e1ce791a
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-16 09:04:18 +0200

    UpdateAM09h04_160523

---
+++ palindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Programme de detection de palindrome dans une phrase");
+
+            Console.Write(" Entrez une phrase se terminant par un point ");
+            string saisiePhrase = Console.ReadLine();
+
+
+            if (saisiePhrase == ".")
+            {
+                Console.WriteLine(" La saisie est inexacte, elle est seulement composée d'un point, nouvelle saisie requise ");
+            }
+
+            if (saisiePhrase.Length == 0)
+            {
+                Console.WriteLine(" La saisie est vide ");
+            }
+            else
+            {
+
+                int phraseLongueur = saisiePhrase.Length;
+                for (int i = 0; i < phraseLongueur / 2; i++)
+                {
+                    if (saisiePhrase[i] != saisiePhrase[phraseLongueur - 1 - i])
+                    {
+                        Console.WriteLine(" La chaîne de caractère n'est pas un palindrome");
+                    }
+                }
+                Console.WriteLine(" La chaîne de caractères est un palindrome ");
+            }
+        }
+    }
+
+}
