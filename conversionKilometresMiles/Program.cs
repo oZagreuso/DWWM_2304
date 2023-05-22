9821c58a63f6149276b75765b4bf1eba480c6907
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-22 09:51:01 +0200

    UpdateAM09h50_220523

---
+++ conversionKilometresMiles/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace conversionKilometresMiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string saisie;
+            double valeur;
+            bool conditionsSaisieOk = true;        
+            double km = 0;
+            double miles = 0;
+            
+            do
+            {
+                Console.WriteLine(" Programme de conversion de kilomètres en miles ");
+                Console.WriteLine(" Saisir une valeur en Km entre 0,01 et 1 000 000 ");
+                saisie = Console.ReadLine();
+               
+
+                if (saisie == "q")
+                {
+                    Console.WriteLine(" FIN PROGRAMME ");
+                    conditionsSaisieOk = false;
+
+                }
+                else 
+                for (double i =)
+                {
+
+                }
+
+            }
+            while (!conditionsSaisieOk);
+        }
+    }
+}
--- tri_d_unTableau/Program.cs
+++ tri_d_unTableau/Program.cs
@@ -9,29 +9,44 @@ namespace tri_d_unTableau
             int[] triTab = new int[] { 128, 64, 8, 512, 16, 32, 256 };
             Console.WriteLine(" Programme de tri d'un tableau ");
             Console.WriteLine(" Le Tableau initial, de l'index 0 à 6, comportant dans l'ordre 128, 64, 8, 512, 16, 32, 256, se présentera, après tri, ainsi: ");
+            PersonalSort(ref triTab);
+            DisplayTab(triTab);
+            
 
-            for (int i = 0; i < triTab.Length; i++)
+        }
+        public static void PersonalSort(ref int[] _myTable)
+        {
+
+            for (int i = 0; i < _myTable.Length; i++)
             {
                 int indiceMin = i;
 
-                for (int j = i + 1; j < triTab.Length; j++)
+                for (int j = i + 1; j < _myTable.Length; j++)
                 {
-                    if (triTab[j] < triTab[indiceMin])
+                    if (_myTable[j] < _myTable[indiceMin])
                     {
                         indiceMin = j;
                     }
                 }
-                if (indiceMin != i)
-                {
-                    int temp = triTab[i];
-                    triTab[i] = triTab[indiceMin];
-                    triTab[indiceMin] = temp;
-                }
+
+                //   if (indiceMin != i)
+
+                int temp = _myTable[i];
+                _myTable[i] = _myTable[indiceMin];
+                _myTable[indiceMin] = temp;
+                // }
             }
-            for (int k = 0; k < triTab.Length; k++)
+            
+        }
+        public static void DisplayTab(int[]_myTable)
+        {
+            Console.WriteLine(" ------ Affichage du Tableau ------ ");
+            
+            for (int k = 0; k < _myTable.Length; k++)
             {
-                Console.WriteLine(triTab[k]);
+                Console.Write(_myTable[k] + " ");
             }
+            Console.WriteLine(" \n ------FIN------ ");
 
         }
     }
