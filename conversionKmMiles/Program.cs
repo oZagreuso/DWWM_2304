ed3e6e874f6675b27f02788a169e3a0465a28cb1
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-23 15:03:42 +0200

    homeUpdate230523

---
+++ conversionKmMiles/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace conversionKmMiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main()
+            {
+
+                string saisie;
+                bool saisieOk = true;
+                double distanceEnKm;
+                double distanceEnMil;
+
+
+
+
+                do
+                {
+                    Console.Write("Saisir une valeur en kilomètres comprise entre 0,01 et 1 000 000 ou taper pour quitter ");
+                    saisie = Console.ReadLine();
+                }
+                while (saisieOk = true);
+
+                if (saisie == "q")
+                {
+                    Console.WriteLine(" Fin du Programme ");
+                    saisieOk = false;
+                }
+                else
+                {
+
+                    if (double.TryParse(saisie, out distanceEnKm) || distanceEnKm < 0.01 || distanceEnKm > 1000000)
+                    {
+                        Console.WriteLine(" Erreur des saisie!!, Saisir une nouvelle valeur en kilomètres entre 0,01 et 100 000 ");
+                    }
+                    else
+                    {
+                        distanceEnMil = distanceEnKm / 1.609;
+                        Console.WriteLine(saisie + " kilomètres équivaut à " + distanceEnMil + " miles", distanceEnKm, distanceEnMil);
+
+                    }
+
+                }
+
+
+            }
+        }
+    }
+}
