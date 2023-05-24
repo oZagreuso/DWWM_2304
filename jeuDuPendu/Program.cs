dd95beed23bbdfc4f55811c1257d9596e91a6321
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-24 09:49:03 +0200

    UpdateAM09h48_240523

--- jeuDuPendu/Program.cs
+++ jeuDuPendu/Program.cs
@@ -11,13 +11,17 @@
 
             do
             {
-                Console.WriteLine(" JOUEUR 1: choisir un mot de 5 caractères minimum ");
-                choixMot = Console.ReadLine();
+                Console.WriteLine(" --- JOUEUR 1: choisir un mot de 5 caractères minimum --- ");
+                choixMot = Console.ReadLine().ToLower();
+
+                if (choixMot.Length < 5)
+                {
+                    Console.WriteLine(" --- Le mot est trop court, choisir un nouveau mot de 5 caractères minimum --- ");
+                }
             }
             while (choixMot.Length < 5);
 
-
-            bool motOk = true;
+            bool motOk = false;
             int nombreEssais = 6;
 
             char[] motSecret = choixMot.ToCharArray();
@@ -34,20 +38,28 @@
 
             do
             {
-               
 
-              
-               
-                    Console.WriteLine(" Il vous reste " + nombreEssais + " essais ");
+                Console.WriteLine(" --- Le mot SECRET est --- ");
+                Console.WriteLine();
 
+                foreach (char element in motSecret)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(" --- Il vous reste " + nombreEssais + " essais --- ");
+
+                Console.WriteLine();
                 Console.WriteLine(" --- JOUEUR 2: saisir lettre --- ");
+
                 char choixLettre = char.Parse(Console.ReadLine().ToLower());
 
                 bool lettreOk = false;
 
                 for (int i = 1; i < choixMot.Length - 1; i++)
                 {
-                    if (choixMot[i].CompareTo(choixLettre)==0)
+                    if (choixMot[i].CompareTo(choixLettre) == 0)
                     {
                         motSecret[i] = choixLettre;
                         lettreOk = true;
@@ -55,24 +67,32 @@
                 }
 
                 if (lettreOk)
+                {                    
+                    Console.WriteLine(" --- EUREKA, lettre trouvée --- ");
+                  
+                }
+                else
                 {
                     nombreEssais--;
-                    Console.WriteLine(" Il vous reste "+ nombreEssais );
+                    Console.WriteLine(" --- YOU LOSE --- ");
+                    Console.WriteLine();
+                    Console.WriteLine(" --- SAME PLAYER TRY AGAIN --- ");
+                    Console.WriteLine();
+                    Console.WriteLine(" Il vous reste " + nombreEssais + " essais ");
                 }
-                Console.WriteLine(" --- Le mot a deviner est --- ");
 
-                foreach (char element in motSecret)
-                {
-                    Console.Write(element + " ");
-                }
-                
                 for (int i = 1; i < motSecret.Length - 1; i++)
                 {
+                    motOk = true;
+
                     if (motSecret[i] == 95)
                     {
                         motOk = false;
+                        
                     }
-                }          
+
+                }         
+                
             }
             while (nombreEssais > 0 && !motOk);
 
@@ -80,10 +100,11 @@
             {
                 Console.WriteLine(" --- YOU WIN ---");
             }
-            else
+            else if (nombreEssais <= 0)
             {
                 Console.WriteLine(" --- GAME OVER ---");
-                Console.WriteLine(" Le mot mystère était : " + choixMot);
+                Console.WriteLine();
+                Console.WriteLine(" Le mot SECRET était : " + choixMot);
             }
         }
     }
