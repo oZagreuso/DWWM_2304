24b8a15685d0bd373f0e56db03a08c1ac95ecf91
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-05-25 11:24:06 +0200

    UpdateAM11h23_250523

--- tpEmpruntAvecCreationTableauAmortissementModeConsole/Program.cs
+++ tpEmpruntAvecCreationTableauAmortissementModeConsole/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+
+        public static double CalculMensualite(double tauxInteretAnnuel, double capitalEmprunte, int nombreAnneesRemboursement)
+        {
+            double tauxMensuel = tauxInteretAnnuel / 12;
+            int nombreMois = nombreAnneesRemboursement * 12;
+
+            double q = Math.Pow(1 + tauxMensuel, -nombreMois);
+            double mensualite = capitalEmprunte * tauxMensuel / (1 - q);
+
+            return Math.Round(mensualite, 2);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double tauxInteretAnnuel;
+            double capitalEmprunte;
+            int nombreAnneesRemboursement;
+
+            Console.WriteLine(" saisir le taux d'interêt annuel ");
+            tauxInteretAnnuel = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" saisir le montant du capital emprunté ");
+            capitalEmprunte = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" saisir le nombre d'années du remboursement ");
+            nombreAnneesRemboursement = int.Parse(Console.ReadLine());
+
+
+            double mensualite = CalculMensualite(tauxInteretAnnuel, capitalEmprunte, nombreAnneesRemboursement);
+
+            Console.WriteLine("La mensualité constante à rembourser est : " + mensualite);
         }
+
     }
 }
